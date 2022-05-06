0bcb099c392e5d2b083e54e19a75836e672eba13
ijanus <emekamicro@gmail.com>
2022-05-06 09:29:13 +0100

    WIP

--- src/ResultUtils/Result.fs
+++ src/ResultUtils/Result.fs
@@ -16,7 +16,7 @@ open ResultUtils.Portability
 [<RequireQualifiedAccess>]
 module Result =
 
-    let ToFSharpCoreResult res =
+    let toFSharpCoreResult res =
 #if NoDUsAsStructs
         match res with
         | Ok o -> FSharp.Core.Result.Ok o
